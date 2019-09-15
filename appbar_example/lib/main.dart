0faf12a497099ec783819bd6b8b6a62af5d8a2d0
Dicky Ermawan S <wanasaja@gmail.com>
2019-09-15 15:45:37 +0700

    lanjut navigator-multi page screen, appbar gradasi - custom

---
+++ appbar_example/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.adb, color: Colors.white),
+          title: Text('Appbar Example', style: TextStyle(color: Colors.white)),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {},
+            ),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blue, Colors.purple],
+                  begin: FractionalOffset.topLeft,
+                  end: FractionalOffset.bottomRight,
+                ),
+                image: DecorationImage(
+                    image: AssetImage('assets/pattern.png'),
+                    fit: BoxFit.cover,
+                    repeat: ImageRepeat.repeat)),
+          ),
+        ),
+      ),
+    );
+  }
+}
